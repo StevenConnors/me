74b23c6dbe3129a45b587004364ee96ad4f33090
Steven Connors <scconnors23@gmail.com>
2020-05-16 13:07:11 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import { getImages } from '../lib/posts'
+
+export async function getStaticProps() {
+  const imgNames = getImages()
+  return {
+    props: {
+      imgNames
+    }
+  }
+}
+
+export default function Home({ imgNames }) {
+  return (
+    <>
+      <Head>
+        <title>yuji tanaka</title>
+      </Head>
+
+      <h1> yuji tanaka </h1>
+
+      <div> 
+        <div className="container columns">
+          {imgNames.map(imgName => (
+              <picture key={imgName}>
+                <img
+                  src={`/images/${imgName}`}
+                  className=".img"
+                />
+              </picture>
+            ))}
+        </div>
+      </div>
+    </>
+  )
+}
