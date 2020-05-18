3d0ef409b876be3e2d56b1382ebe8e241d1aebaa
Steven Connors <scconnors23@gmail.com>
2020-05-17 23:46:47 -0400

    add wishes on readme

---
+++ components/header.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from './header.module.css'
+
+export default function Header() {
+  return (
+    <>
+      <Head>
+        <title>yuji tanaka</title>
+      </Head>
+      
+      <Link href="/">  
+        <h1>佑治</h1>
+      </Link>
+
+      <Link href="/about">
+        <span className={styles.link}>about</span>
+      </Link>
+      
+      <a className={styles.link} href="https://www.linkedin.com/in/steven-connors/">
+        <span  >英語</span>
+      </a>
+
+    </>
+  )
+}
