2b973ac7c1762a36b3a22c6af91278a6fd7daab0
Steven Connors <scconnors23@gmail.com>
2020-06-25 09:45:01 -0400

    hopefully legit bbuild?

--- pages/img/[filename].js
+++ pages/img/[filename].js
@@ -1,55 +1,69 @@
-import { useRouter } from 'next/router'
-import styles from '../../components/imageGallery.module.css'
-import { getImageDescription, getAllImageNames } from "../../lib/staticDataFetcher"
-import Tag from '../../components/tag'
+// import { useRouter } from 'next/router'
+// import styles from '../../components/imageGallery.module.css'
+// import { getImageDescription, getAllImageNames } from "../../lib/staticDataFetcher"
+// import Tag from '../../components/tag'
+// import Header from '../../components/header'
+
 import Header from '../../components/header'
 
-// Define for the router what the valid [filename]s are. 
-// We use the value from /public/images for valid filenames.
-export async function getStaticPaths() {
-  const paths = getAllImageNames().map(imgName => ({
-    params: { filename: imgName },
-  }))
-
-  return {
-    paths: paths,
-    fallback: false 
-  };
-}
-
-// Obtain the props to render.
-export async function getStaticProps({ params }) {
-  const imageDescription = getImageDescription(params.filename)
-  return {
-    props: {
-      imageDescription
-    }
-  } 
-}
-
-export default function ImageInfo({ imageDescription}) {
-  const router = useRouter()
-  const { filename } = router.query
+export default function About() {
   return (
     <>
       <Header />
-
-      <div className={`${styles.container} ${styles.columns}`}>
-        <img
-          src={`/images/${filename}`}
-          className={styles.imgDetailed}
-        />
-
-        <p>{imageDescription.content}</p>
+      
+      <div>
+        NYI
       </div>
-
-      {/* Render tags */}
-      {imageDescription.tags.map(tag => (
-          <Tag tag={tag} />
-      ))}
-
-    {/* <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-79.97377395629884%2C40.413365349070865%2C-79.91197586059572%2C40.475680726483795&amp;layer=mapnik&amp;marker=40.444530259240274%2C-79.94287490844727"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=40.4445&amp;mlon=-79.9429#map=13/40.4445/-79.9429">View Larger Map</a></small> */}
     </>
   )
 }
+
+// // Define for the router what the valid [filename]s are. 
+// // We use the value from /public/images for valid filenames.
+// export async function getStaticPaths() {
+//   const paths = getAllImageNames().map(imgName => ({
+//     params: { filename: imgName },
+//   }))
+
+//   return {
+//     paths: paths,
+//     fallback: false 
+//   };
+// }
+
+// // Obtain the props to render.
+// export async function getStaticProps({ params }) {
+//   const imageDescription = getImageDescription(params.filename)
+//   return {
+//     props: {
+//       imageDescription
+//     }
+//   } 
+// }
+
+// export default function ImageInfo({ imageDescription}) {
+//   const router = useRouter()
+//   const { filename } = router.query
+//   return (
+//     <>
+//       <Header />
+
+//       <div className={`${styles.container} ${styles.columns}`}>
+//         <img
+//           src={`/images/${filename}`}
+//           className={styles.imgDetailed}
+//         />
+
+//         <p>{imageDescription.content}</p>
+//       </div>
+
+//       {/* Render tags */}
+//       {imageDescription.tags.map(tag => (
+//           <Tag tag={tag} />
+//       ))}
+
+//     {/* <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-79.97377395629884%2C40.413365349070865%2C-79.91197586059572%2C40.475680726483795&amp;layer=mapnik&amp;marker=40.444530259240274%2C-79.94287490844727"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=40.4445&amp;mlon=-79.9429#map=13/40.4445/-79.9429">View Larger Map</a></small> */}
+//     </>
+//   )
+// }
 
--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -2,6 +2,18 @@ import { getAllTags, getAllImageNamesWithTag } from "../../lib/staticDataFetcher
 import Header from '../../components/header'
 import ImageGallery from '../../components/imageGallery'
 
+export default function About() {
+  return (
+    <>
+      <Header />
+      
+      <div>
+      NYI
+      </div>
+    </>
+  )
+}
+
 // Define for the router what the valid [tag]s are. We use the value from /public/images for valid filenames.
 // export async function getStaticPaths() {
 //   const paths = getAllTags().map(tag => ({
