5de2ced89cb63035cb4c669a8a91780ec5430f23
Steven Connors <scconnors23@gmail.com>
2020-06-25 22:12:21 -0400

    update cloudinary url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     CLOUDINARY_CLOUD_NAME: "dwsenj1bp",
     CLOUDINARY_API_KEY: "853685646649433",
     CLOUDINARY_API_SECRET: "oZOXsiWgAcuYNqkYkV1PzmdKbAI",
-    assetPrefix: isProd ? 'http://res.cloudinary.com' : 'http://res.cloudinary.com',
+    assetPrefix: isProd ? 'https://res.cloudinary.com' : 'https://res.cloudinary.com',
   },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
