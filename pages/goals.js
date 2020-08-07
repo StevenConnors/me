80ccb58552ca2baf1e180a2c1c5133d636fea5d2
Steven Connors <scconnors23@gmail.com>
2020-08-06 23:30:31 -0400

    start goals website

---
+++ pages/goals.js
@@ -0,0 +1,15 @@
+import Header from '../components/header'
+import styles from '../styles/utils.module.css'
+
+export default function About() {
+  return (
+    <>
+      <Header />
+
+      <div>
+        Its been over a 1/3 of a year in this pandemic. Here are my goals for the rest of this crazy 2020.
+      </div>
+
+    </>
+  )
+}
