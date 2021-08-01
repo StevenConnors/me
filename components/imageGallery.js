dc1ed9e78a5c8c0e979fd983a23dfeb415a8e981
StevenConnors <scconnors23@gmail.com>
2021-08-01 19:39:51 -0400

    minor cleanup

--- components/imageGallery.js
+++ components/imageGallery.js
@@ -4,7 +4,6 @@ import styles from './imageGallery.module.css'
 export default function ImageGallery({ imgNameToThumbNail }) {
   return (
     <>
-      <div> Hello!</div>
       <div className={`${styles.container} ${styles.columns}`}>
         {imgNameToThumbNail.map((imgNameToUrl, i) => (
             <Link href={`/img/${imgNameToUrl.imgName}`} key={imgNameToUrl.thumbNailUrl}> 
