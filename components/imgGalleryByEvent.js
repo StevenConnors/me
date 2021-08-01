15cabb86922d85d961a40cd1c5d60ffb342d9a94
StevenConnors <scconnors23@gmail.com>
2021-08-01 17:17:26 -0400

    Got the event date sorting correct.

--- components/imgGalleryByEvent.js
+++ components/imgGalleryByEvent.js
@@ -4,10 +4,7 @@ import styles from './imgGalleryByEvent.module.css'
 export default function ImageGalleryByEvent({ events }) {
   return (
     <>
-
-      {events.map((event, i) => {
-        console.log("HI", event);
-       
+      {events.map((event) => {       
         return (
           <>
             <h2>{event.eventDescription.title}</h2>
--- eventDescriptions/Sedona_AZ.md
+++ eventDescriptions/Sedona_AZ.md
@@ -1,5 +1,6 @@
 ---
 title: 'Sedona, AZ'
+date: "2021-05-15"
 ---
 
 Sedona, AZ, May 2021.
