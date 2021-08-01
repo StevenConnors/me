e73ed30d7b746a21eaa72cef029b9963ab6f0d8b
StevenConnors <scconnors23@gmail.com>
2021-08-01 17:17:26 -0400

    Got the event date sorting correct.

--- eventDescriptions/JacksonHole_WY.md
+++ eventDescriptions/JacksonHole_WY.md
@@ -1,5 +1,6 @@
 ---
 title: 'Jackson Hole, WY'
+date: "2021-06-15"
 ---
 
 Jackson Hole, WY, June 2021.
--- pages/byEvents.js
+++ pages/byEvents.js
@@ -9,7 +9,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      events: await getPhotosByEvents(BY_EVENTS_DIR),
+        events: await getPhotosByEvents(BY_EVENTS_DIR),
     }
   }
 }
