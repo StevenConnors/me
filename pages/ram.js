6472dbbff7d96f856486cd74e68d680b665cd882
StevenConnors <scconnors23@gmail.com>
2021-08-01 17:48:01 -0400

    update dir name;

--- pages/ram.js
+++ pages/ram.js
@@ -20,6 +20,9 @@ export default function ByEvents({ events }) {
       <Header />
 
       <br></br>
+      <br></br>
+      my random access memory
+      <br></br>
 
       <ImageGalleryByEvent events={events} />
     </>
