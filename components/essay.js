9c4412b55d58a4f5ded7dc85ecdaf9de0fd033d4
StevenConnors <scconnors23@gmail.com>
2021-12-13 00:18:50 -0500

    under construction

---
+++ components/essay.js
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from './essay.module.css'
+
+export default function Essay() {
+  return (
+    <div className={styles.essayContainer}>
+
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+
+        12-13-2021
+        <br></br>
+
+        <br></br>
+        Let me share my past year with you.
+        <br></br>
+
+
+
+{/* 
+        I type on this screen, 70 characters wide, my thoughts of the past year. How I ended up back on the same little island, New York New York. You could fold my past 2 years in half and I'd find myself not too far off, appearing as if the pandemic hadn't fased him. Yet I know its not the same. The energy of the people here, the awareness of normalcy which once was taken away from the city. But, I know that's mostly bullshit because what really changed was me. In the last 20 months or so I grew a lot, and today I hope I can share some thoughts.
+        <br></br>
+
+        For far too long I've been a little beta. I wish I was like the fish. My first 2 and a half years out of college were rough man. I felt like I had 0 support network in a new city, didn't know anybody & having a personality that makes it difficult for me to go out & meet people wasn't a great combination to have & an environment to be in. For the longest time, until recently in fact, I always viewed this past as a shameful part of my life. That I need to live with the fact that I smoked "one's best years of your life" away. Its only recently that I feel like I've been able to accept that it was a necessary time period, for me to recover & regain strength to face the world again.
+        <br></br>
+
+        I type on this scren, 70 characters wide, my memories of the Steelers game. In an unusually warm December 4pm matchup against the rivals Baltimore, I wore 4 layers w/ a heattech & was sweating my balls off. I will have to say its better than the alternative of being underdressed in a blizzard match. Whenever the players line up for each play, I can help but notice the bright white lights reflecting off of their dark helmets, flickering in my eyes as they all hunch down to form a line of white light ray, and for a second, silence wraps the field as the players all take their final inhale, before brutalising their bodies once again, as the crowd cheers on, for glory, for victory, for conquest, for superiority. Isn't that what it is end the end? Each of us finding our own little yardstick to measure one's value against - being tossed & turned over factors that you can't control. Where we went wrong was that the yardsticks were never the real goal. It was a way to measure the goal. Monkey see monkey do.
+        <br></br>
+
+        I type on this screen, 70 characters wide, because I can distinctly see each letter as I type them out on my split mechanical keyboard, ErgodoxEZ. Each letter of each word of each sentence of each paragraph means something, even though if its just 1 of 1000. If it weren't for this next letter, t, the rest of the paper wouldn't exist. Every letter counts, speak, write, and think like it, so that you don't unnecessarily waste somebody else's time.
+        <br></br>
+
+        I miss the conversations we used to have, the ones laden with alcohol. Just the two of us, above a falafel store, talking about whatever came to our minds because everything we said was a chance to learn about the other person. I recall when you first looked at me with a strongly disapproving look, "I think that's lame". I can never predict what the next words are. I'm always on edge. I walked by a bookstore, entered it, & picked up a book to read. It had a nice cover & the first few chapters were interesting. I recently bought a new audiobook.
+        <br></br>
+
+        On the timely topic of books tho, e inspired me so much this year to continue reading. I tried reading a few times after college, but found out that my reading skills end at the amazon listing page. I'm good at reading things to order but never good at reading the things I order. You inspired me to read & finish Atlas Shrugged & I think I finished at least 5+ other books this year. Thank you so much e. You continue to help me expand. (Also audiobooks are a godsend. I can just listen to Obama read his book on repeat.)
+        <br></br>
+
+
+        Another thing that e helped me is to be kind to myself. If a girl as amazing as her is somehow kind to me, then maybe I should try to be kind to myself. I know that its not easy for some people, myself included, but it's kinda nice to be nice to yourself. Occasionally I wake up feeling like I have nothing to worry about and that life is going just fine, that I am in control and I am doing what I want to be. I hope I can feel that more often.
+        <br></br>
+
+        I typed on this screen, 70 characters wide, my thoughts of the past year. How much did you know already? How much was new? What old memory peeked to say hi? Although digitally these characters could go on ad infinitum, with the abundance of electricity and magnets recording the randomest of phenomena - a step taken, a photo liked, a website visited, a phone call made - I hope that the electronic footsteps left behind with these words will be a marking, however small, of my existence, created by me.
+        <br></br>
+
+        Just because a memory wasn't written doesn't mean it's not important. I still cherish it every day. Just like my friends that I may not see any more. Know that I cherish our memories even though we may no longer speak. 
+
+ */}
+
+
+
+
+
+    </div>
+  )
+}
