4a2e5bd94e78cac652faed75d6d69cdee36b8c0e
StevenConnors <scconnors23@gmail.com>
2022-05-06 17:19:38 -0400

    Re-start w/ just the 5-7 page

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    customKey: 'my-value',
-    CLOUDINARY_CLOUD_NAME: "dwsenj1bp",
-    CLOUDINARY_API_KEY: "853685646649433",
-    CLOUDINARY_API_SECRET: "oZOXsiWgAcuYNqkYkV1PzmdKbAI",
-    assetPrefix: isProd ? 'https://res.cloudinary.com' : 'https://res.cloudinary.com',
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-  crossOrigin: 'anonymous',
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,110 @@
+
+   
 import Header from '../components/header'
-import ImageGallery from '../components/imageGallery'
-import cloudinary from "cloudinary";
+// import ImageGallery from '../components/imageGallery'
+// import cloudinary from "cloudinary";
 
-export async function getServerSideProps() {
-  let urls = await getImageNameToThumbNailUrl(ISE_DIR);
+// export async function getServerSideProps() {
+//   let urls = await getImageNameToThumbNailUrl(ISE_DIR);
   
-  return {
-    props: {
-        imgNameToThumbNail: urls,
-    }
-  }
-}
+//   return {
+//     props: {
+//         imgNameToThumbNail: urls,
+//     }
+//   }
+// }
 
-export default function Home({ imgNameToThumbNail }) {
+import Head from 'next/head'
+import clientPromise from '../lib/mongodb'
+
+export default function Home({ isConnected }) {
   return (
     <>
       <Header />
 
-      <ImageGallery imgNameToThumbNail={imgNameToThumbNail} />
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+
+      {/* <ImageGallery imgNameToThumbNail={imgNameToThumbNail} /> */}
     </>
   )
 }
 
-export const ISE_DIR = "yuji/ise";
 
-export async function getImageNameToThumbNailUrl(folder) {
-  let cloudinaryUrls = await getImageUrls(folder);
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
 
-  const imgNameToThumbNailUrl = cloudinaryUrls.map(imgJson => {
-    let filename = imgJson.filename;
-    let imgSuffix = filename.substring(0, filename.lastIndexOf("_"));
-    let thumbNailUrl = decreaseImageQualityURL(removeUrlPrefix(imgJson));
-
-    return {imgName: imgSuffix, thumbNailUrl: thumbNailUrl};
-  });
-  return imgNameToThumbNailUrl;
-  // Return json of {IMG_1244: ThumbNailUrlForIMG_1244}
-}
-
-export async function getImageUrls(folder) {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET   
-  });
-
-  let res = cloudinary.v2.search
-    .expression(`folder=${folder}/*`)
-    .sort_by('public_id','desc')
-    .execute()
-    // .max_results(500)
-    .then(result=> {
-      return result.resources; 
-    });    
-  return res;
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
 
 
-function removeUrlPrefix(imgJson) {
-  return imgJson.secure_url.substring('https://res.cloudinary.com'.length);
-}
 
-function decreaseImageQualityURL(url) {
-  let uploadIndex = url.indexOf("upload/");
-  let beforeTransformationString = url.substring(0, uploadIndex + "upload/".length);
-  let afterTransformationString = url.substring(uploadIndex + "upload/".length);
-  let lQurl = `${beforeTransformationString}q_20/${afterTransformationString}`;
-  return lQurl
-};
+// export const ISE_DIR = "yuji/ise";
+
+// export async function getImageNameToThumbNailUrl(folder) {
+//   let cloudinaryUrls = await getImageUrls(folder);
+
+//   const imgNameToThumbNailUrl = cloudinaryUrls.map(imgJson => {
+//     let filename = imgJson.filename;
+//     let imgSuffix = filename.substring(0, filename.lastIndexOf("_"));
+//     let thumbNailUrl = decreaseImageQualityURL(removeUrlPrefix(imgJson));
+
+//     return {imgName: imgSuffix, thumbNailUrl: thumbNailUrl};
+//   });
+//   return imgNameToThumbNailUrl;
+//   // Return json of {IMG_1244: ThumbNailUrlForIMG_1244}
+// }
+
+// export async function getImageUrls(folder) {
+//   cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key: process.env.CLOUDINARY_API_KEY, 
+//     api_secret: process.env.CLOUDINARY_API_SECRET   
+//   });
+
+//   let res = cloudinary.v2.search
+//     .expression(`folder=${folder}/*`)
+//     .sort_by('public_id','desc')
+//     .execute()
+//     // .max_results(500)
+//     .then(result=> {
+//       return result.resources; 
+//     });    
+//   return res;
+// }
+
+
+// function removeUrlPrefix(imgJson) {
+//   return imgJson.secure_url.substring('https://res.cloudinary.com'.length);
+// }
+
+// function decreaseImageQualityURL(url) {
+//   let uploadIndex = url.indexOf("upload/");
+//   let beforeTransformationString = url.substring(0, uploadIndex + "upload/".length);
+//   let afterTransformationString = url.substring(uploadIndex + "upload/".length);
+//   let lQurl = `${beforeTransformationString}q_20/${afterTransformationString}`;
+//   return lQurl
+// };
