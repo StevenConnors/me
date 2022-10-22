f76db38eeb8767ab088fc6aef5f7d2dd9ef1e447
StevenConnors <scconnors23@gmail.com>
2022-10-22 14:42:18 +0900

    asdf

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   outputFileTracing: false,
   staticPageGenerationTimeout: 300,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Header from '../components/header'
+import Image from 'next/image'
 
 export default function About() {
   return (
@@ -11,6 +12,13 @@ export default function About() {
       <div>
         The name 佑治 means  &#39;heal the person to your right&#39;. Whoever you may be, I hope I can be that person for you.
       </div>
+
+      <Image
+        src="https://res.cloudinary.com/dwsenj1bp/image/upload/v1666357771/new/IMG_7197_kzfdbv.jpg"
+        alt="asdfasdfsadf of a triangle"
+        width={500}
+        height={500}
+      />
     </>
   )
 }
