1e7064fc1ab4474e4e9791ab598e2c6cee5725d4
StevenConnors <scconnors23@gmail.com>
2022-10-22 16:15:18 +0900

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Header from '../components/header'
+import { useState, useEffect } from 'react';
+import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/header'
 
-export default function About() {
+export default function About({images}) {
   return (
     <>
       <Header />
@@ -13,12 +15,52 @@ export default function About() {
         The name 佑治 means  &#39;heal the person to your right&#39;. Whoever you may be, I hope I can be that person for you.
       </div>
 
-      <Image
-        src="https://res.cloudinary.com/dwsenj1bp/image/upload/v1666357771/new/IMG_7197_kzfdbv.jpg"
-        alt="asdfasdfsadf of a triangle"
-        width={500}
-        height={500}
-      />
+      <ul >
+          {images.map(image => {
+            return (
+              <li key={image.id}>
+                <a href={image.link} rel="noreferrer">
+                  <div>
+                    <Image width={image.width} height={image.height} src={image.image} alt="" />
+                  </div>
+                  <h3 >
+                    { image.title }
+                  </h3>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
+    headers: {
+      Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+    }
+  }).then(r => r.json());
+
+  console.log({results});
+
+  
+  const { resources } = results;
+
+  const images = resources.map(resource => {
+    const { width, height } = resource;
+    return {
+      id: resource.asset_id,
+      title: resource.public_id,
+      image: resource.secure_url,
+      width,
+      height
+    }
+  });
+
+  return {
+    props: {
+      images
+    }
+  }
 }
